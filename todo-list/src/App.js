ba43b46057287fbbea7f861b12375114b651735a
Alexandro Valdez <alexandroivaldez@gmail.com>
2022-09-03 13:02:48 -0500

    Added addTask()
    Adds a task to the task list

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -8,16 +8,40 @@ class App extends Component {
     super();
 
     this.state = {
-
+      tasks: [
+        {
+          taskId: 1,
+          taskContext: "Clean the dishes"
+        },
+        {
+          taskId: 2,
+          taskContext: "Clean your room"
+        }
+      ]
     }
+  }
+
+  //Adds task to state
+  addTask= () => {
+    const updatedArr = [...this.state.tasks, {taskId: 3, taskContext: "hello"}];
+    this.setState(() => {
+      return {tasks: updatedArr}
+    });
+    console.log("Button pressed!");
   }
 
 
   render() {
     return (
       <div className="App">
-        <h1> ToDo List </h1>
+        <br></br>
         <input type="text" placeholder='Add a task...' />
+        <button onClick={this.addTask}>ENTER</button>
+        {
+          this.state.tasks.map((task) => {
+            return <h3>{task.taskContext}</h3>
+          })
+        }
       </div>
     );
   }
