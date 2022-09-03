e58f409a5c7212691e99c73128af307d89c81557
Alexandro Valdez <alexandroivaldez@gmail.com>
2022-09-03 15:23:31 -0500

    Added functionality to <InputBox />

--- todo-list/src/components/card-list/card-list.component.jsx
+++ todo-list/src/components/card-list/card-list.component.jsx
@@ -5,10 +5,23 @@ class CardList extends Component {
 
         return (
             <div>
-
+                {
+                    this.props.taskProps.map((task) => {
+                        return (
+                        <div key={task.taskId} className="card">
+                            <h3>{task.taskContext}</h3>
+                            <button onClick={event => this(task.taskId)} id="done">DONE</button>
+                        </div>
+                        )
+                    })
+                }
             </div>
         )
     }
 }
 
 export default CardList;
+
+
+
+//<CardList removeTaskHandler={this.removeTask} taskProps={this.state.tasks}/>
