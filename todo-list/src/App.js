db7cc1864444f5b912418272f927fcaed0142a77
Alexandro Valdez <alexandroivaldez@gmail.com>
2022-09-05 16:35:38 -0500

    Updated CSS & removed Enter btn

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -65,7 +65,7 @@ class App extends Component {
             return (
             <div key={task.taskId}  id="cardz">
               <span class="text">{task.taskContext}</span>
-              <span class="icon">
+              <span class="icon" onClick={event => this.removeTask(task.taskId)} >
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" >
                   <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" ></path>
                 </svg>
